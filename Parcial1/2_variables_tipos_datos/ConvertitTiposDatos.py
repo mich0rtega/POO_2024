1a784ad3bbbaa4313145c4cb104ea9924e80ec5c
mich0rtega <156623518+mich0rtega@users.noreply.github.com>
2024-05-14 12:12:59 -0600

    ConvertirTiposDatos

--- Parcial1/2_variables_tipos_datos/ConvertitTiposDatos.py
+++ Parcial1/2_variables_tipos_datos/ConvertitTiposDatos.py
@@ -7,3 +7,37 @@
 #Concatenar un str con str
 texto = "Soy una cadema de texto"
 print(texto+"soy otra cadena de texto")
+
+
+#Concatenar un int a str
+numero = 23
+numero_str = str(numero)
+#misma forma de convertir
+print("el numero:"+numero_str)
+print("el numero:"+str(numero))
+
+#Comcatenar un str a int
+
+n1= '23'
+n2= 33
+
+#ES MEJOR HACER EL CAMBIO DENTRO DE LAS EXPRESIONES
+suma = int(n1) + n2
+print("El numero:"+str(suma))
+
+
+#Comcatenar un Float y Int a str
+
+n1= '23'
+n2= 33.0
+
+suma = int(n1) + n2
+print("El numero:"+str(int(suma)))
+print(f"el numero:"(int(suma)))
+
+#Comcatenar un Float y float a float
+n1= '23.34'
+n2= '33.99'
+
+suma = float(n1) + n2
+print(f"el numero:"(int(suma)))

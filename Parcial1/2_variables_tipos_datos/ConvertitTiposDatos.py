a351c34d640b992e43124e15e28f1d90776d76fa
mich0rtega <156623518+mich0rtega@users.noreply.github.com>
2024-05-14 10:30:54 -0600

    Create ConvertitTiposDatos.py

---
+++ Parcial1/2_variables_tipos_datos/ConvertitTiposDatos.py
@@ -0,0 +1,9 @@
+""" 
+   Comentario de varias lineas
+   Nota: a la hora de concatenar cadenas no es posible incluir en algunas ocasiones contenido de variables que no sean de tipo str
+
+"""
+
+#Concatenar un str con str
+texto = "Soy una cadema de texto"
+print(texto+"soy otra cadena de texto")
